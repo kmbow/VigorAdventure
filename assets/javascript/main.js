bd5ffa7a724a44bf2d2b166fc8e09591c878d8ae
David Gutierrez <DavidGutierrez@Davids-MacBook.local>
2017-08-25 10:00:02 -0500

    Created the firebase and added the intialization to main.js and linked it via index.html

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCPDq9zGMsYIYATgUKuilErUgrDFOCSPXw",
+    authDomain: "vigoradventure.firebaseapp.com",
+    databaseURL: "https://vigoradventure.firebaseio.com",
+    projectId: "vigoradventure",
+    storageBucket: "vigoradventure.appspot.com",
+    messagingSenderId: "894124047169"
+  };
+
+  firebase.initializeApp(config);
